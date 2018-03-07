9c3ba74585532041438a96c77b575386312aaf78
神楽坂覚々 <komeiji@satori.moe>
2018-03-08 01:43:04 +0800

    init

---
+++ Demo.php
@@ -0,0 +1,19 @@
+<?php
+include 'WebsocketRPC.php';
+$rpc = new WebsocketRPC(9998, 9999, [
+	'host' => "127.0.0.1",
+	'port' => 6379,
+	'prefix' => "WSRPC_",
+]);
+$rpc->WebsocketOpen(function ($user_key) {
+	print_r($user_key);
+});
+$rpc->WebsocketMessage(function ($data) use ($rpc) {
+	$rpc->WebsocketSend($data['user_key'], "Recv:" . $data['data']);
+	print_r($data);
+});
+$rpc->WebSocketClose(function ($user_key) {
+	print_r($user_key);
+});
+$rpc->HttpServerStart();
+$rpc->WebsocketStart();
